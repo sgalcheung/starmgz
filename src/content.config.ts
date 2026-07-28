c64c41f44e03e0b9b7ddc9aef0e7ae89d5b7f572
Sgal Cheung <zhangsigao@live.com>
2026-07-28 17:37:32 +0800

    Add magazine schema definitions and refactor collections to use new schema

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,6 +3,7 @@ import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
 import { file } from 'astro/loaders';
 import { z } from 'astro/zod';
+import { MagazineIssueSchema } from './data/magazine';
 
 const docs = defineCollection({
   loader: docsLoader(),
@@ -47,58 +48,14 @@ const docs = defineCollection({
   }),
 });
 
-const magazineSchema = z.object({
-  // 年份
-  year: z.number(),
-
-  // 期数
-  issueNo: z.number(),
-
-  // 卷号（总期号）
-  volumeNo: z.number(),
-
-  // 发布日期
-  publishDate: z.coerce.date(),
-
-  // 杂志目录
-  sections: z
-    .array(
-      z.object({
-        // 栏目标题
-        title: z.string(),
-
-        // 栏目副标题（文章总标题）
-        subTitle: z.string().optional(),
-
-        // 栏目文章
-        items: z.array(
-          z.object({
-            // 页码
-            page: z.number().or(z.string()),
-
-            // 标题
-            title: z.string(),
-
-            // 作者
-            author: z.string().optional(),
-
-            // 跳转链接
-            link: z.string().optional(),
-          }),
-        ),
-      }),
-    )
-    .optional(),
-});
-
 const zgjjjcs = defineCollection({
   loader: file('./src/data/zgjjjcs.json'),
-  schema: magazineSchema,
+  schema: MagazineIssueSchema,
 });
 
 const cpajs = defineCollection({
   loader: file('./src/data/cpajs.json'),
-  schema: magazineSchema,
+  schema: MagazineIssueSchema,
 });
 
 export const collections = {
---
+++ src/data/magazine.ts
@@ -0,0 +1,66 @@
+import { z } from 'astro/zod';
+
+/**
+ * 单篇文章
+ */
+export const MagazineItemSchema = z.object({
+  // 页码
+  page: z.number().or(z.string()),
+
+  // 标题
+  title: z.string(),
+
+  // 作者
+  author: z.string().optional(),
+
+  // 跳转链接
+  link: z.string().optional(),
+});
+
+/**
+ * 栏目
+ */
+export const MagazineSectionSchema = z.object({
+  // 栏目标题
+  title: z.string(),
+
+  // 栏目副标题（文章总标题）
+  subTitle: z.string().optional(),
+
+  // 栏目文章
+  items: z.array(MagazineItemSchema),
+});
+
+/**
+ * 一期杂志
+ */
+export const MagazineIssueSchema = z.object({
+  id: z.string(),
+  // 年份
+  year: z.number(),
+
+  // 期数
+  issueNo: z.number(),
+
+  // 卷号（总期号）
+  volumeNo: z.number(),
+
+  // 发布日期
+  publishDate: z.coerce.date(),
+
+  // 杂志目录
+  sections: z.array(MagazineSectionSchema),
+});
+
+/**
+ * 整本杂志
+ */
+export const MagazineSchema = z.array(MagazineIssueSchema);
+
+export type MagazineItem = z.infer<typeof MagazineItemSchema>;
+
+export type MagazineSection = z.infer<typeof MagazineSectionSchema>;
+
+export type MagazineIssue = z.infer<typeof MagazineIssueSchema>;
+
+export type Magazine = z.infer<typeof MagazineSchema>;
